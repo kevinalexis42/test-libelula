73ea93e90d9569b3245ff22f1fd3dc2f7f62dd55
kevinalexis42 <ka51823@hotmail.com>
2026-03-08 18:44:44 -0500

    fix: corregido errores de CI para front

--- frontend/src/app/policy/[id]/page.tsx
+++ frontend/src/app/policy/[id]/page.tsx
@@ -61,6 +61,7 @@ export default function PolicyDetailPage({ params }: PolicyDetailPageProps) {
     if (isAuthenticated) {
       loadPolicy();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params.id, isAuthenticated]);
 
   if (!isAuthenticated) {
--- frontend/src/app/quote/[id]/page.tsx
+++ frontend/src/app/quote/[id]/page.tsx
@@ -43,6 +43,7 @@ export default function QuoteDetailPage({ params }: QuoteDetailPageProps) {
 
   useEffect(() => {
     loadQuote();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params.id]);
 
   const handleEmitPolicy = async () => {
