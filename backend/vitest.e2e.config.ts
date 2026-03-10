8db2f6384f3e06ae1a77c7ace5d40be289a2fdfa
kevinalexis42 <ka51823@hotmail.com>
2026-03-09 19:19:03 -0500

    fix:corregido errores de CI back

--- backend/vitest.e2e.config.ts
+++ backend/vitest.e2e.config.ts
@@ -1,13 +1,19 @@
+import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 import { resolve } from 'path';
 
 export default defineConfig({
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'node',
     include: ['test/**/*.e2e-spec.ts'],
-    hookTimeout: 30000,
-    testTimeout: 30000,
+    hookTimeout: 60000,
+    testTimeout: 60000,
     deps: {
       interopDefault: true,
     },
